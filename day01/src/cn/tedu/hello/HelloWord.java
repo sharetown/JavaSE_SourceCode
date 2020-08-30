83cf395d91df4dda183490b610af611874c99689
sharetown <2267949319@qq.com>
2020-08-30 19:28:58 +0800

    HelloWorld

---
+++ day01/src/cn/tedu/hello/HelloWord.java
@@ -0,0 +1,10 @@
+package cn.tedu.hello;
+
+import java.nio.channels.NetworkChannel;
+
+public class HelloWord {
+	public static void main(String[] args) {
+		System.out.print("HelloWorld");
+		System.out.println("HelloWorld");
+	}
+}
