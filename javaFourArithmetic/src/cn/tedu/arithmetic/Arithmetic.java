abc43e69cd213c0adc1a9944d551296b3b94e5e7
sharetown <2267949319@qq.com>
2020-08-31 21:26:49 +0800

    Java运算符及四则运算

---
+++ javaFourArithmetic/src/cn/tedu/arithmetic/Arithmetic.java
@@ -0,0 +1,15 @@
+package cn.tedu.arithmetic;
+
+/**
+ * 四则运算
+ * 
+ * @author sharetown
+ * @date 2020年8月3日 上午11:01:59
+ */
+public class Arithmetic {
+	public static void main(String[] args) {
+		// 1、计算结果的数据类型，与最大类型保持一致
+		// 2、byte、short、char三种比int小的整数，运算时会先自动转换成int
+
+	}
+}
